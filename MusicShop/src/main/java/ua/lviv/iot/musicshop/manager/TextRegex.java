e538baad2a02cc09688999a90de250f2a9667a7d
Ustym Buchko <ustym.buchko1@gmail.com>
2022-05-10 16:33:19 +0300

    custom length of words

--- MusicShop/src/main/java/ua/lviv/iot/musicshop/manager/TextRegex.java
+++ MusicShop/src/main/java/ua/lviv/iot/musicshop/manager/TextRegex.java
@@ -6,20 +6,18 @@ import java.util.regex.Pattern;
 public class TextRegex {
 
   public String textEdit(String text, int length) {
-    Pattern pattern = Pattern.compile("\\b[\\w&&[^aeiouAEIOU\\s]]\\w{3}\\b");
+    Pattern pattern = Pattern.compile("\\b[\\w&&[^aeiouAEIOU\\s]]\\w{" + (length - 1) + "}\\b");
     Matcher matcher = pattern.matcher(text);
     while (matcher.find()) {
       text = text.replace(matcher.group(), "");
     }
     Pattern whitespacesDelete = Pattern.compile("[\\W]");
     int i = 0;
-    length = text.length();
-    while (i < length) {
+    while (i < text.length()) {
       if ((text.charAt(i) == ' ' && i == 0) ||
           (text.charAt(i) == ' ' && whitespacesDelete.matcher(text.substring(i + 1, i + 2))
               .matches())) {
         text = text.substring(0, i) + text.substring(i + 1);
-        length -= 1;
       } else {
         i += 1;
       }
